973320e093296ce082cfaad61a9cf89308b596c4
rohitnairtech <puthenrohit@gmail.com>
2020-12-28 09:02:50 +0530

    removed the use of deprecated methods, destructured unneccesary objects and used const instead of var for type/data immutability

--- api/api.basketItems.js
+++ api/api.basketItems.js
@@ -1,5 +1,5 @@
-var expressSession = require("express-session");
-var requestJson = require("request-json");
+const expressSession = require("express-session");
+const requestJson = require("request-json");
 
 module.exports = function attachHandlers(router) { //, passport) {
     // get requests
@@ -7,41 +7,34 @@ module.exports = function attachHandlers(router) { //, passport) {
     router.get('/api/basketItems/', list);
 };
 
-var list = function(req, res) {
-
-    var sess = req.session;
-    if (sess.products) {
-        return res.json(sess.products);
-    } else {
-        return res.json({});
-    }
+const list = ({session}, res) => {
+    return res.json((session.products) ? session.products : {});
 };
 
-var addItem = function(req, res) {
+const addItem = ({params, session}, res) => {
 
-    var productId = req.params.productId;
+    const {productId} = params;
 
-    var client = requestJson.newClient('http://127.0.0.1:5000/');
+    const client = requestJson.Client('http://127.0.0.1:5000/');
 
     client.get('api/products/' + productId, function(err, result, data) {
         if (err) {
             return res.send(500);
         }
 
-        var productInfo = {
+        const productInfo = {
             "productId": data._id,
             "name": data.name,
             "price": data.offers.price
         };
 
 
-        var sess = req.session;
-        if (!sess.products) {
-            sess.products = new Array();
+        if (!session.products) {
+            session.products = new Array();
         }
-        sess.products.push(productInfo);
+        session.products.push(productInfo);
         return res.send({
-            ItemCount: sess.products.length
+            ItemCount: session.products.length
         });
 
         //return console.log(body.rows[0].title);
