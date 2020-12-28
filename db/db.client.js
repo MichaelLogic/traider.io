7a516ccb85c3b4f658da0209feb4beefccd95040
rohitnairtech <puthenrohit@gmail.com>
2020-12-28 10:03:05 +0530

    Updated with latest method names and improved code readability

--- db/db.client.js
+++ db/db.client.js
@@ -1,26 +1,27 @@
 
-var mongo = require("mongodb");
-var MongoClient = mongo.MongoClient,
-    Server = require('mongodb').Server,
-    BSON = mongo.BSONPure;
+const {MongoClient, ObjectID} = require("mongodb");
 
-var ObjectId = mongo.ObjectID
 
-exports.getDbClient = function() {
-    return new MongoClient(new Server("127.0.0.1", 27017), {
-        native_parser: true
-    });
-};
+exports = {
+    getDbClient : () => new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true }),
+    dbHost : () => "127.0.0.1:27017",
+    dbName : () => "traider",
+    makeObjectID : () => new ObjectID(id)
+}
 
-exports.dbHost = function() {
-return "127.0.0.1:27017";
-};
+// exports.getDbClient = function() {
+//     return new MongoClient("mongodb://localhost:27017", { useNewUrlParser: true, useUnifiedTopology: true });
+// };
 
-exports.dbName = function() {
-    return "traider";
-};
+// exports.dbHost = function() {
+// return "127.0.0.1:27017";
+// };
 
-exports.makeObjectID = function(id) {
-    //return new BSON.ObjectID(id);
-    return new ObjectId(id);
-};
+// exports.dbName = function() {
+//     return "traider";
+// };
+
+// exports.makeObjectID = function(id) {
+//     //return new BSON.ObjectID(id);
+//     return new ObjectID(id);
+// };
